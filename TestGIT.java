85c3eb9586b9a2c47671a9f1666e629b558e7372
FMJ <f.jablecki@poczta.fm>
2020-02-22 21:16:15 +0100

    changes 1

--- TestGIT.java
+++ TestGIT.java
@@ -2,6 +2,8 @@
 class TestGIT{
 	
 	public static void main(String args[]){
+		public TestGIT2 testObj2 = new TestGIT2();
+		System.out.println(testObj2.string1);
 		System.out.println("Elo test kurwa");
 	}
 	
---
+++ TestGIT2.java
@@ -0,0 +1,4 @@
+class TestGIT2{
+	public String string1 = "Just a generic text.";
+	
+}
